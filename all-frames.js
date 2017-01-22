21bf2ba22881d0ea7ea8f937c3a9b5f05072f706
Jannis R <mail@jannisr.de>
2017-01-22 18:56:49 +0100

    implementation :clap:

--- all-frames.js
+++ all-frames.js
@@ -2,19 +2,27 @@
 
 const through = require('through2')
 
-const lastFrame = (type = null) => {
+const allFrames = (type = null) => {
+	const cache = {}
+
 	return through.obj(function (data, _, cb) {
 		if (!data.gestures || data.gestures.length === 0) return cb()
 
 		for (let gesture of data.gestures) {
 			if (type && gesture.type !== type) continue
+			const id = gesture.id.toString()
+
+			if (gesture.state === 'start') cache[id] = [gesture]
+			else cache[id].push(gesture)
+
 			if (gesture.state === 'stop') {
-				this.emit(gesture.type, gesture)
-				this.push(gesture)
+				this.emit(gesture.type, cache[id])
+				this.push(cache[id])
+				delete cache[id]
 			}
 		}
 		cb()
 	})
 }
 
-module.exports = lastFrame
+module.exports = allFrames
